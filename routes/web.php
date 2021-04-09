04a16e3b37b29a592202970e1d406b4065c35e20
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-09 14:10:32 +0100

    Deleting Data

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,11 @@ Route::get('/update', function () {
     //$post->tags()->attach($tag);
     $post->tags()->sync([1,2]);
 });
+
+Route::get('/delete', function () {
+    $post = Post::findOrFail(2);
+
+    foreach($post->tags as $tag){
+        $tag->whereId(1)->delete();
+    }
+});
